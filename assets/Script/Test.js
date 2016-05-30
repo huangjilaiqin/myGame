5c213d8c75a1179f76ce2cff6c656e84efbbcf8b
huangjilaiqin <1577594730@qq.com>
2016-05-30 23:56:39 +0800

    联通网络，添加声音，添加类

---
+++ assets/Script/Test.js
@@ -0,0 +1,6 @@
+cc.Class({
+    extends: cc.Component,
+    foo:function(){
+        cc.log('foo');
+    },
+});
--- assets/Script/main.js
+++ assets/Script/main.js
@@ -1,3 +1,7 @@
+
+const Test = require('Test');
+const Network = require('Network');
+
 cc.Class({
     extends: cc.Component,
 
@@ -11,11 +15,21 @@ cc.Class({
         //    readonly: false,    // optional, default is false
         // },
         // ...
+        bgAudio:{
+            default:null,
+            url:cc.AudioClip
+        },
     },
 
     // use this for initialization
     onLoad: function () {
-
+        cc.log(Test);
+        var test = new Test();
+        test.foo();
+        var network = new Network();
+        //cc.log(network.getInstance(undefined,undefined,function(){cc.log('getInstance');}));
+        cc.log(network.getInstance());
+        cc.audioEngine.playMusic(this.bgAudio, true);
     },
 
     // called every frame, uncomment this function to activate update callback
